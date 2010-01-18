44fdc0d3c388aa7395b475a1d35df329959cba78
younix <web2p10@wemelug.de>
2010-01-18 21:52:10 +0100

    hello world => $src_content

--- prototyp/body.inc.php
+++ prototyp/body.inc.php
@@ -3,7 +3,7 @@
 require_once("php/template_builder.php");
 
 $page = new template_builder("template/main.html");
-$page->set_att("content", "Hallo Welt!");
+$page->set_att("content", $src_content);
 
 echo $page->get_src();
 
